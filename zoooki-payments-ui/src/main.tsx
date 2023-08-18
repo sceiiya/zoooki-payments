c005e6b979ba9219ed2e8c7501c9462784265870
sceiiya <124661065+sceiiya@users.noreply.github.com>
2023-08-18 11:19:47 +0800

    include all styling files

--- zoooki-payments-ui/src/main.tsx
+++ zoooki-payments-ui/src/main.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './App.css'
+import './App-md.css'
+import './App-sm.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
