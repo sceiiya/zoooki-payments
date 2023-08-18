d17d48e559f05be16adfeda78d21214ee0c28518
sceiiya <124661065+sceiiya@users.noreply.github.com>
2023-08-18 11:19:32 +0800

    initiate product interface

---
+++ zoooki-payments-ui/src/types/interface.tsx
@@ -0,0 +1,12 @@
+export interface product {
+    collection: string;
+    title: string;
+    type: string;
+    price: number;
+    stocks: number;
+    sold: number;
+    description: string;
+    image: string;
+    artist: string;
+    artistlink: string;
+}
