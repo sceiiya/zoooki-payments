716f68607b0d93207390b8e79feb1bdf31566b18
sceiiya <124661065+sceiiya@users.noreply.github.com>
2023-08-18 19:29:53 +0800

    debug

--- zoooki-payments-ui/src/components/NavBar.tsx
+++ zoooki-payments-ui/src/components/NavBar.tsx
@@ -1,5 +1,5 @@
 // import { Link } from "react-router-dom"
-import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap'
+import { Navbar, Nav, Container } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import wLogo from '../assets/icons/n-logo-b.png'
 
@@ -21,21 +21,6 @@ const NavBar = () =>
                         <Nav.Link as={Link} to="/contacts">
                             Contacts
                         </Nav.Link>
-                        <Nav.Link as={Link} to="/success">
-                            Success
-                        </Nav.Link>     
-                        <Nav.Link href="#link">Link</Nav.Link>
-                        {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">
-                                Another action
-                            </NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">
-                                Separated link
-                            </NavDropdown.Item>
-                        </NavDropdown> */}
                     </Nav>
                 </Navbar.Collapse>
             </Container>
