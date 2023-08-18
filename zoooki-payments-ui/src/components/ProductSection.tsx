4020563fefb2ebdac0e9c0a025b97fb4389d6f9d
sceiiya <124661065+sceiiya@users.noreply.github.com>
2023-08-18 11:20:08 +0800

    render products details

--- zoooki-payments-ui/src/components/ProductSection.tsx
+++ zoooki-payments-ui/src/components/ProductSection.tsx
@@ -1,24 +1,112 @@
+import { useQuery } from "react-query"
+import { FaTwitter } from 'react-icons/fa'
+
+import imgBiboo from "../assets/images/bibooZeroEgg.jpg"
+import imgShiorin from "../assets/images/shioriSaiha.jpg"
+import imgFuwamoco from "../assets/images/fuwamocoQ12.jpg"
+import imgBijou from "../assets/images/bibooDecrilus.jpg"
+
+import { product } from "../types/interface"
+
 const ProductSection = ({}) =>
 {
-    const products: number[] = [1, 2, 3];
-    return <>
+
+    const products: product[] = [
+        {
+            collection: "T-shirt Collection: Emotion Elegance Series",
+            title: "Radiant Embrace Tee",
+            type: "T-shirt",
+            price: 2500,
+            stocks: 900,
+            sold: 0,
+            description: "Immerse yourself in the brilliance of emotions with the Radiant Embrace Tee, a design inspired by Koseki Bijou. This shirt captures the essence of Bijou's enchanting persona, blending elegance and emotion seamlessly.",
+            image: imgBiboo,
+            artist: "E G G",
+            artistlink: "https://twitter.com/zero0ix"
+        },
+        {
+            collection: "Notebook Collection: Chronicles of Novella Notebook",
+            title: "Whimsical Tales: Shiori's Chronicles Notebook Set",
+            type: "Notebook Set",
+            price: 3000,
+            stocks: 900,
+            sold: 0,
+            description: "Chronicle your own tales with the Chronicles of Novella Notebook set. Each notebook is adorned with Shiori Novella's favorite stories, allowing you to pen your thoughts and dreams within the pages of history.",
+            image: imgShiorin,
+            artist: "saiha",
+            artistlink: "https://twitter.com/318_Saiha"
+        },
+        {
+            collection: "Accessories: Abyssgard Chaos Charms",
+            title: "Twins' Playful Enchantment",
+            type: "Acrylic Standee",
+            price: 4000,
+            stocks: 900,
+            sold: 0,
+            description: "Channel the whimsical chaos of the Abyssgard twins with the Whimsical Chaos Charms. These adorable accessories showcase their energetic personalities and are perfect for adding a touch of charm to your belongings.",
+            image: imgFuwamoco,
+            artist: "Q12",
+            artistlink: "https://twitter.com/q12_flamingo"
+        },
+        {
+            collection: "Keychain: Radiant Emotion Keychain",
+            title: "Twins' Playful Enchantment",
+            type: "Keychain",
+            price: 2000,
+            stocks: 900,
+            sold: 0,
+            description: "Carry the brilliance of emotions with the Radiant Emotion Keychain, inspired by Koseki Bijou. This limited-edition keychain features a dazzling crystalline pendant, reflecting Bijou's radiant persona. Elevate your daily life with this enchanting collector's piece that captures the magic of emotions.",
+            image: imgBijou,
+            artist: "Decrilus",
+            artistlink: "https://twitter.com/decrilus"
+        }
+    ];
+
+    return (
         <div>
-            {products.map((_, index) => (
-                <section
-                    key={index}
-                    className={`container d-flex productSection ${
-                    index % 2 === 0 ? 'full-bleed' : ''
-                }`}
-                >
-                    <div className={`d-flex ${
-                    index % 2 === 0 ? 'flex-row-reverse' : 'flex-row'
-                }`}>
-                        {/* ... */}
-                    </div>
-                </section>
-            ))}
+            {products.map((product, index) => {
+                const {
+                    image,
+                    title,
+                    collection,
+                    type,
+                    price,
+                    stocks,
+                    sold,
+                    description,
+                    artist,
+                    artistlink
+                } = product
+    
+                return (
+                    <section
+                        key={index}
+                        className={`container d-flex productSection ${
+                            index % 2 === 0 ? 'full-bleed' : ''
+                        }`}
+                    >
+                        <div className={`d-flex p-4 `}>
+                            <div className="productImage">
+                                <img src={image} alt='product' height="100%" width="100%" />
+                            </div>
+                            <div className="productDetails">
+                                <h2>{title}</h2>
+                                <h3>{collection}</h3>
+                                <p>Artist:&nbsp;<a href={artistlink}><FaTwitter />&nbsp;{artist}</a></p>
+                                <p>Type: {type}</p>
+                                <p>Price: ¥&nbsp;{price}</p>
+                                <p>Stocks: {stocks}</p>
+                                <p>Sold: {sold}</p>
+                                <p>{description}</p>
+                                <button>Add to Bag</button>
+                                <button>Buy Now</button>
+                            </div>
+                        </div>
+                    </section>
+                );
+            })}
         </div>
-    </>
+    );
 }
 
 export default ProductSection
