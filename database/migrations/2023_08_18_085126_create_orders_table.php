6595a6b1d23c94a778166bd97870fbfe3a95621a
sceiiya <124661065+sceiiya@users.noreply.github.com>
2023-08-19 03:28:46 +0800

    fix order migration

--- database/migrations/2023_08_18_085126_create_orders_table.php
+++ database/migrations/2023_08_18_085126_create_orders_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
+            $table->string('status');
+            $table->integer('total_price');
+            $table->string('session_id');
             $table->timestamps();
         });
     }
