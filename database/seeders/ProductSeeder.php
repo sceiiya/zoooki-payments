de476327f6db0c11b656438753841738c1da7a63
sceiiya <124661065+sceiiya@users.noreply.github.com>
2023-08-18 17:54:18 +0800

    set product model controller factory seeder

---
+++ database/seeders/ProductSeeder.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Product;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // $productData = [
+        //     [
+        //         'collection' => "T-shirt Collection: Emotion Elegance Series",
+        //         'title' => "Radiant Embrace Tee",
+        //         'type' => "T-shirt",
+        //         'price' => 2500,
+        //         'stocks' => 900,
+        //         'description' => "Immerse yourself in the brilliance of emotions with the Radiant Embrace Tee...",
+        //         'image' => 'bibooZeroEgg.jpg',
+        //         'artist' => "E G G",
+        //         'artistlink' => "https://twitter.com/zero0ix",
+        //     ], 
+        //     [
+        //         'collection' => "Notebook Collection: Chronicles of Novella Notebook",
+        //         'title' => "Whimsical Tales: Shiori's Chronicles Notebook Set",
+        //         'type' => "Notebook Set",
+        //         'price' => 3000,
+        //         'stocks' => 900,
+        //         'description' => "Chronicle your own tales with the Chronicles of Novella Notebook set...",
+        //         'image' => 'shioriSaiha.jpg',
+        //         'artist' => "saiha",
+        //         'artistlink' => "https://twitter.com/318_Saiha",
+        //     ],
+        //     [
+        //         'collection' => "Accessories: Abyssgard Chaos Charms",
+        //         'title' => "Twins' Playful Enchantment",
+        //         'type' => "Acrylic Standee",
+        //         'price' => 4000,
+        //         'stocks' => 900,
+        //         'description' => "Channel the whimsical chaos of the Abyssgard twins with the Whimsical Chaos Charms...",
+        //         'image' => 'fuwamocoQ12.jpg',
+        //         'artist' => "Q12",
+        //         'artistlink' => "https://twitter.com/q12_flamingo",
+        //     ],
+        //     [
+        //         'collection' => "Keychain: Radiant Emotion Keychain",
+        //         'title' => "Twins' Playful Enchantment",
+        //         'type' => "Keychain",
+        //         'price' => 2000,
+        //         'stocks' => 900,
+        //         'description' => "Carry the brilliance of emotions with the Radiant Emotion Keychain...",
+        //         'image' => 'bibooDecrilus.jpg',
+        //         'artist' => "Decrilus",
+        //         'artistlink' => "https://twitter.com/decrilus",
+        //     ],       
+        // ];
+
+        // foreach ($productData as $productDetails) {
+        //     Product::create($productDetails);
+        // }
+    }
+}
