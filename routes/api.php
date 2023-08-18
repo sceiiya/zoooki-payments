329eb822b55bd582aa1f1bda7aaa35842f67ddf9
sceiiya <124661065+sceiiya@users.noreply.github.com>
2023-08-19 03:30:12 +0800

    add proper routes

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
+use Illuminate\Support\Facades\Redirect;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,5 +20,18 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::get('/', function () {
+    sleep(2);
+    return Redirect::to('https://zoooki-collab.wd49p.com/');
+});
+Route::get('/redirect-to-home', function () {
+    return Redirect::to('https://zoooki-collab.wd49p.com/');
+})->name('redirectToHome');
+
 Route::get('/products', [ProductController::class, 'index'])->name('product.inv');
 Route::get('/products/{id}', [ProductController::class, 'show'])->name('product.details');
+
+Route::post('/checkout', [ProductController::class, 'checkout'])->name('checkout');
+Route::get('/success', [ProductController::class, 'success'])->name('checkout.success');
+Route::get('/cancel', [ProductController::class, 'cancel'])->name('checkout.cancel');
+Route::post('/stripe-webhook', [ProductController::class, 'webhook'])->name('checkout.webhook');
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    sleep(2);
+    return Redirect::to('https://sceiiyadev.netlify.app/');
 });
