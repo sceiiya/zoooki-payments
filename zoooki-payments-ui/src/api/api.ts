e4252af7ed742bace38f9bbdfcaad3674f627cab
sceiiya <124661065+sceiiya@users.noreply.github.com>
2023-08-19 13:08:38 +0800

    debug test allow cors

--- zoooki-payments-ui/src/api/api.ts
+++ zoooki-payments-ui/src/api/api.ts
@@ -1,7 +1,39 @@
-import axios from "axios";
+// import axios from "axios";
 
-// export function checkout ({bag, currentDomain}: {bag: number[], currentDomain: string })
+// // export function checkout ({bag, currentDomain}: {bag: number[], currentDomain: string })
+// // {
+// //     return axios.post(
+// //         'http://127.0.0.1:8000/api/checkout',
+// //         { 
+// //             bag: bag,
+// //             domain: currentDomain },
+// //         {
+// //             headers: {
+// //                 'Content-Type': 'application/json',
+// //             },
+// //         })
+// //         .then(res => res)
+// // }
+
+// export const checkout = async bagged =>
+// //  ({bag, currentDomain}: {bag: number[], currentDomain: string })
 // {
+//     const res = await fetch('https://api-zoooki-collab.wd49p.com/api/checkout', {
+//         method: 'POST',
+//         body: JSON.stringify({
+//             bag: bag,
+//             domain: currentDomain 
+//         }),
+//         headers: {
+//             'Content-Type': 'application/json',
+//         }
+//     })
+//     return res.json()
+        
+//     })
+
+
+//     })
 //     return axios.post(
 //         'http://127.0.0.1:8000/api/checkout',
 //         { 
@@ -14,35 +46,3 @@ import axios from "axios";
 //         })
 //         .then(res => res)
 // }
-
-export const checkout = async bagged =>
-//  ({bag, currentDomain}: {bag: number[], currentDomain: string })
-{
-    const res = await fetch('http://127.0.0.1:8000/api/checkout', {
-        method: 'POST',
-        body: JSON.stringify({
-            bag: bag,
-            domain: currentDomain 
-        }),
-        headers: {
-            'Content-Type': 'application/json',
-        }
-    })
-    return res.json()
-        
-    })
-
-
-    })
-    return axios.post(
-        'http://127.0.0.1:8000/api/checkout',
-        { 
-            bag: bag,
-            domain: currentDomain },
-        {
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        })
-        .then(res => res)
-}
