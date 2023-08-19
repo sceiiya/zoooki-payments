d2e64c03436552cc46b0a42cde97e859172ae0f7
sceiiya <124661065+sceiiya@users.noreply.github.com>
2023-08-19 13:26:07 +0800

    use axios in usemutate

--- zoooki-payments-ui/src/components/bag-buttons/QuickBuyButton.tsx
+++ zoooki-payments-ui/src/components/bag-buttons/QuickBuyButton.tsx
@@ -1,21 +1,20 @@
 import React from "react";
 import { BagProps } from "../../types/interface";
 import { useMutation } from "react-query";
+import axios from "axios"; // Import Axios
 
 const QuickBuyBttn: React.FC<BagProps> = ({ bag, addToBag, productId }) => {
   const currentDomain = window.location.origin;
 
   const checkoutMutation = useMutation(
     () =>
-      fetch("https://api-zoooki-collab.wd49p.com/api/checkout/", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ bag: bag, domain: currentDomain }),
-      }).then(response => response.json()), // Parse the JSON response
+      axios.post("https://api-zoooki-collab.wd49p.com/api/checkout/", {
+        bag: bag,
+        domain: currentDomain,
+      }),
     {
-      onSuccess: (data) => {
+      onSuccess: (response) => {
+        const data = response.data;
         console.log(data);
         // Access the required data from 'data' object
         // const checkoutUrl = data.checkout_url;
